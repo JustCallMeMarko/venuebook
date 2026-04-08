9caa473483c10c511bd8d5948ca670f1960b9d46
JustCallMeMarko <cantimbuhanmark.stnick@gmail.com>
2026-04-08 21:26:03 +0800

    added database config

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost"; // change to the deployed db
+    $db_name = "syncly"; // change to the deployed db
+    $username = "root"; // change to the deployed db
+    $password = ""; // change to the deployed db
+    $conn = ""; 
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo json_encode(["status" => "error", "message" => "Database connection failed"]);
+    }
+?>
